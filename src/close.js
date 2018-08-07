9e2372f02fa4a3b8b4eeeaf8f72af33f56e5dc94
CarisAvi21 <shinnytinystar@hotmail.com>
2018-08-07 13:08:06 -0500

    modificando home.html

--- src/close.js
+++ src/close.js
@@ -1,5 +1,5 @@
 const container = document.getElementById('container');
 setTimeout(function() {
   container.classList.add('close');
-  window.location.href = '../src/index.html'
+  window.location.href = '../src/views/home.html';
 }, 9000);
